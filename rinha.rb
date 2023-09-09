db8f8e7410c9e639c525a7de9e7aef745c85242e
Daniel Hilst <danielhilst@gmail.com>
2023-09-09 19:06:13 -0300

    Add python compilation

--- rinha.rb
+++ rinha.rb
@@ -55,44 +55,44 @@ def eval_(node, env)
   end
 end
 
-def indent(text, n)
-  text.split("\n").map do |text|
-    "#{' ' * n * 2}#{text}"
+def indent(text, n, ts)
+  text.to_s.split("\n").map do |text|
+    "#{' ' * n * ts}#{text}"
   end.join("\n")
 end
 
-def compile(node)
+def compile_to_ruby(node)
   case node
   in {kind: "Let", name: { text: function_name }, value: { kind: "Function", parameters:, value: body }, next: next_ }
     parameters = parameters.map {|x| x[:text]}.join(",")
     <<~EOS
     def #{function_name}(#{parameters})
-    #{indent(compile(body), 1)}
+    #{indent(compile_to_ruby(body), 1, 2)}
     end
 
-    #{compile(next_)}
+    #{compile_to_ruby(next_)}
     EOS
   in {kind: "Let", name: { text: }, value:, next: next_ }
     <<~EOS
-    #{text} = #{compile(value)}
-    #{compile(next_)}
+    #{text} = #{compile_to_ruby(value)}
+    #{compile_to_ruby(next_)}
     EOS
   in {kind: "Function", parameters:, value: body}
     pnames = parameters.map {|par| par[:text]}
-    "->(#{pnames.join(',')}){ #{compile(body)} }"
+    "->(#{pnames.join(',')}){ #{compile_to_ruby(body)} }"
   in {kind: "Print", value: }
-    "puts(#{compile(value)})"
+    "puts(#{compile_to_ruby(value)})"
   in {kind: "Call", callee:, arguments:}
-    callee = compile(callee)
-    arguments = arguments.map {|arg| compile(arg) }.join(",")
+    callee = compile_to_ruby(callee)
+    arguments = arguments.map {|arg| compile_to_ruby(arg) }.join(",")
     "#{callee}(#{arguments})"
   in {kind: "Var", text: name}
     name
   in {kind: "Int", value: value}
     value
   in {kind: "Binary", lhs:, op:, rhs:}
-    lhs = compile(lhs)
-    rhs = compile(rhs)
+    lhs = compile_to_ruby(lhs)
+    rhs = compile_to_ruby(rhs)
     op_mapping = {
       Eq: "==",
       Or: "||",
@@ -105,10 +105,10 @@ def compile(node)
     "#{lhs} #{op} #{rhs}"
   in {kind: "If", condition:, then: then_, otherwise:}
     <<~EOS
-      if #{compile(condition)}
-        #{compile(then_)}
+      if #{compile_to_ruby(condition)}
+        #{compile_to_ruby(then_)}
       else
-        #{compile(otherwise)}
+        #{compile_to_ruby(otherwise)}
       end
     EOS
   else
@@ -116,9 +116,96 @@ def compile(node)
   end
 end
 
+def compile_to_python(node, context=nil)
+  case node
+  in {kind: "Let", name: { text: function_name }, value: { kind: "Function", parameters:, value: body }, next: next_ }
+    parameters = parameters.map {|x| x[:text]}.join(",")
+    <<~EOS
+    def #{function_name}(#{parameters}):
+    #{indent(compile_to_python(body, :function_body), 1, 4)}
+
+    #{compile_to_python(next_)}
+    EOS
+  in {kind: "Let", name: { text: }, value:, next: next_ }
+    <<~EOS
+    #{text} = #{compile_to_python(value)}
+    #{compile_to_python(next_, context)}
+    EOS
+  in {kind: "Function", parameters:, value: body}
+    pnames = parameters.map {|par| par[:text]}
+    case context
+    when :function_body
+      "return (lambda #{pnames.join(', ')}: #{compile_to_python(body)})"
+    else
+      "(lambda #{pnames.join(', ')}: #{compile_to_python(body)})"
+    end
+  in {kind: "Print", value: }
+    case context 
+    when :function_body
+      "return print(#{compile_to_python(value)})"
+    else
+      "print(#{compile_to_python(value)})"
+    end
+  in {kind: "Call", callee:, arguments:}
+    callee = compile_to_python(callee)
+    arguments = arguments.map {|arg| compile_to_python(arg) }.join(", ")
+    case context
+    when :function_body
+      "return #{callee}(#{arguments})"
+    else
+      "#{callee}(#{arguments})"
+    end
+  in {kind: "Var", text: name}
+    case context
+    when :function_body
+      "return #{name}"
+    else
+      name
+    end
+  in {kind: "Int", value: value}
+    case context
+    when :function_body
+      "return #{value}"
+    else
+      value
+    end
+  in {kind: "Binary", lhs:, op:, rhs:}
+    lhs = compile_to_python(lhs)
+    rhs = compile_to_python(rhs)
+    op_mapping = {
+      Eq: "==",
+      Or: "or",
+      Sub: "-",
+      Add: "+",
+      Lt: "<",
+    }
+    op = op_mapping[op.to_sym]
+
+    case context
+    when :function_body
+      "return #{lhs} #{op} #{rhs}"
+    else 
+      "#{lhs} #{op} #{rhs}"
+    end
+  in {kind: "If", condition:, then: then_, otherwise:}
+    <<~EOS
+      if #{compile_to_python(condition)}:
+      #{indent(compile_to_python(then_, context), 1, 4)}
+      else:
+      #{indent(compile_to_python(otherwise, context), 1, 4)}
+    EOS
+  else
+    fail "unexpected node #{node}"
+  end
+end
+
+
 case ARGV[0]
 when "compile_to_ruby"
-  print(compile(JSON.parse(STDIN.read, symbolize_names: true)[:expression]))
+  print(compile_to_ruby(JSON.parse(STDIN.read, symbolize_names: true)[:expression]))
+when "compile_to_python"
+  print(compile_to_python(JSON.parse(STDIN.read, symbolize_names: true)[:expression]))
+
 when "eval"
   eval_(JSON.parse(STDIN.read, symbolize_names: true)[:expression], {})
 else
--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
+import os
 from setuptools import setup
 from Cython.Build import cythonize
 
+files = ["_crinha.pyx"]
+
+if os.path.exists("./_rinha_python_out.pyx"):
+    files += ["_rinha_python_out.pyx"]
+
+print(files)
+
 setup(
-    ext_modules=cythonize("_crinha.pyx", annotate=True),
+    ext_modules=cythonize(files, annotate=True),
 )
