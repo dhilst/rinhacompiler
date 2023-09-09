3ea008ca40f0e88e104796bd29e3d996c62af681
Daniel Hilst <danielhilst@gmail.com>
2023-09-09 18:27:08 -0300

    Compile to ruby

--- rinha.rb
+++ rinha.rb
@@ -55,4 +55,72 @@ def eval_(node, env)
   end
 end
 
-eval_(JSON.parse(ARGF.read, symbolize_names: true)[:expression], {})
+def indent(text, n)
+  text.split("\n").map do |text|
+    "#{' ' * n * 2}#{text}"
+  end.join("\n")
+end
+
+def compile(node)
+  case node
+  in {kind: "Let", name: { text: function_name }, value: { kind: "Function", parameters:, value: body }, next: next_ }
+    parameters = parameters.map {|x| x[:text]}.join(",")
+    <<~EOS
+    def #{function_name}(#{parameters})
+    #{indent(compile(body), 1)}
+    end
+
+    #{compile(next_)}
+    EOS
+  in {kind: "Let", name: { text: }, value:, next: next_ }
+    <<~EOS
+    #{text} = #{compile(value)}
+    #{compile(next_)}
+    EOS
+  in {kind: "Function", parameters:, value: body}
+    pnames = parameters.map {|par| par[:text]}
+    "->(#{pnames.join(',')}){ #{compile(body)} }"
+  in {kind: "Print", value: }
+    "puts(#{compile(value)})"
+  in {kind: "Call", callee:, arguments:}
+    callee = compile(callee)
+    arguments = arguments.map {|arg| compile(arg) }.join(",")
+    "#{callee}(#{arguments})"
+  in {kind: "Var", text: name}
+    name
+  in {kind: "Int", value: value}
+    value
+  in {kind: "Binary", lhs:, op:, rhs:}
+    lhs = compile(lhs)
+    rhs = compile(rhs)
+    op_mapping = {
+      Eq: "==",
+      Or: "||",
+      Sub: "-",
+      Add: "+",
+      Lt: "<",
+    }
+    op = op_mapping[op.to_sym]
+
+    "#{lhs} #{op} #{rhs}"
+  in {kind: "If", condition:, then: then_, otherwise:}
+    <<~EOS
+      if #{compile(condition)}
+        #{compile(then_)}
+      else
+        #{compile(otherwise)}
+      end
+    EOS
+  else
+    fail "unexpected node #{node}"
+  end
+end
+
+case ARGV[0]
+when "compile_to_ruby"
+  print(compile(JSON.parse(STDIN.read, symbolize_names: true)[:expression]))
+when "eval"
+  eval_(JSON.parse(STDIN.read, symbolize_names: true)[:expression], {})
+else
+  fail "Usage: ruby rinha.rb {compile|eval} < input.json"
+end
